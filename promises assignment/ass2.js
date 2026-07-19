847f85dd9319768ca425436ebf0ac0d3b5cfe629
snehal_reddy_ldp <snehalreddythathireddy@gmail.com>
2026-07-19 10:33:11 +0530

    Promises Assignment

---
+++ promises assignment/ass2.js
@@ -0,0 +1,27 @@
+const printName = name => "Hi" + name;
+console.log(printName("Snehal"));
+const printBill = (name,bill) => `Hi ${name}, please pay ${bill}`;
+console.log(printBill("snehal",300))
+const person = {
+    name: "Noam Chomsky",
+    age: 92
+}
+const {name,age} = person;
+console.log(name);
+console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
