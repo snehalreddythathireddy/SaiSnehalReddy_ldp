8f05accd57a8b6c8a7cb93e7d560d6635deb1feb
snehalreddythathireddy <snehalreddythathireddy@gmail.com>
2026-07-29 16:40:35 +0530

    Update fetchUserEmail.js

--- promises assignment/fetchUserEmail.js
+++ promises assignment/fetchUserEmail.js
@@ -1,12 +1,15 @@
-const getData = (uId, callback) => {
-  setTimeout(() => {
-    console.log(`Fetched the data for user: ${uId}`);
-    callback("skc@gmail.com");
-  }, 4000);
+const fetchUserEmail = (uid) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Fetched data for ${uid}`);
+            resolve(`${uid}@gmail.com`);
+        }, 4000);
+    });
 };
-
-console.log("Start");
-getData("skc", (email) => {
-  console.log("Email id of the user is: " + email);
-});
-console.log("End");
+const displayUserEmail = async () => {
+    console.log("Start");
+    const email = await fetchUserEmail("skc");
+    console.log("Email id of the user is:", email);
+    console.log("End");
+};
+displayUserEmail();
