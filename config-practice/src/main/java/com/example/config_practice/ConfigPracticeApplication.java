dc5c9d2dea46c54a0f2b672ef6c5b35d033c3f8d
snehal_reddy_ldp <snehalreddythathireddy@gmail.com>
2026-09-15 17:43:28 +0530

    Add config practice

---
+++ config-practice/src/main/java/com/example/config_practice/ConfigPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.config_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigPracticeApplication.class, args);
+	}
+
+}
---
+++ config-practice/src/test/java/com/example/config_practice/ConfigPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.config_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
