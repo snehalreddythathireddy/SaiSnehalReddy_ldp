19da7ddd2d9e90ab781e2b6926a8228dfa12f0d2
snehal_reddy_ldp <snehalreddythathireddy@gmail.com>
2026-09-22 07:57:26 +0530

    Add student web app

---
+++ student-webapp/src/test/java/com/example/student_webapp/StudentWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
