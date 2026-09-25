57cb56ce70e5cda1b3230b99f40e139d2b7b463f
snehal_reddy_ldp <snehalreddythathireddy@gmail.com>
2026-09-25 10:43:03 +0530

    Add Maven assignment 1: package Spring Boot app as jar

---
+++ maven-assignment-1/src/main/java/com/example/maven_assignment_1/MavenAssignment1Application.java
@@ -0,0 +1,13 @@
+package com.example.maven_assignment_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAssignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenAssignment1Application.class, args);
+	}
+
+}
---
+++ maven-assignment-1/src/test/java/com/example/maven_assignment_1/MavenAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maven_assignment_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenAssignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
